f820c4e25ac5c0c4776c0514b9b1077432763696
Samuel Li <shaomenglee@gmail.com>
2015-07-03 15:18:43 -0700

    Add BinManipulation

--- BinManipulation/Print1st10Floats.cpp
+++ BinManipulation/Print1st10Floats.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdio>
 
 using namespace std;
 
 int main( int argc, char* argv[] )
 {
-    string name = argv[1];
     float buf[10];
-    FILE* f = fopen( name.c_str(), "rb" );
-    fseek( f, 256*256 - 5, SEEK_SET );
+    FILE* f = fopen( argv[1], "rb" );
     fread( buf, sizeof(float), 10, f );
     fclose( f );
     for( int i = 0; i < 10; i++ )
