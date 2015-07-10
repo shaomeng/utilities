1f61d91397c31fb5fa00e718f36dbc6177325657
Samuel Li <shaomenglee@gmail.com>
2015-07-10 10:53:50 -0700

    finished the zero out program

--- BinManipulation/ZeroOutFloats.cpp
+++ BinManipulation/ZeroOutFloats.cpp
@@ -10,71 +10,125 @@
  *
  */
 
-#include "libQccPack.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <algorithm>
 
-#define USG_STRING "%s:inputSPBT %d:cratio %s:outputSPBT"
+using namespace std;
 
-float FindThreshold( int cratio, QccWAVSubbandPyramid3D* pyramid )
+/* 
+ * Opens a file to read and returns the length of the file in byte.
+ */
+long int OpenFileRead( FILE* &file, const char* filename )
 {
-    long size = (pyramid -> num_frames) * (pyramid -> num_rows) * (pyramid -> num_cols);
-    if( size % cratio != 0 )
-        printf(" coeffs size not divided by cratio!\n" );
-    long n = size / cratio - 1;     // the nth largest, indexing from 1.
+    file = fopen( filename, "rb" );
+    if( file == NULL ) {
+        cerr << "File open error: " << filename << endl;
+        exit(1);
+    }
+    fseek( file, 0, SEEK_END );
+    long size = ftell( file );
+    fseek( file, 0, SEEK_SET );
 
-    std::vector<float> allCoeffs;
-    for (int frame = 0; frame < pyramid->num_frames; frame++)
-        for (int row = 0; row < pyramid->num_rows; row++)
-            for (int col = 0; col < pyramid->num_cols; col++)
-                if( pyramid->volume[frame][row][col] > 0 )
-                    allCoeffs.push_back( -1.0 * pyramid->volume[frame][row][col] );
-                else
-                    allCoeffs.push_back( pyramid->volume[frame][row][col] );
+    return size;
+}
 
-    std::nth_element( allCoeffs.begin(), allCoeffs.begin()+n, allCoeffs.end() );
-    float nth = -1.0 * allCoeffs[n];
+/*
+ * Opens a file to write.
+ */
+int OpenFileWrite( FILE* &file, const char* filename )
+{
+    file = fopen( filename, "wb" );
+    if( file == NULL ) {
+        cerr << "Open file to write error: " << filename << endl;
+        return 1;
+    }
+    return 0;
+}
+
+/*
+ * Read a data chunk from the binary file.
+ * Both offset and count are in the number of floats (not bytes).
+ */
+int ReadChunk( FILE* &file, long offset, long count, float* buf )
+{
+    long rt = fseek( file, sizeof(float) * offset, SEEK_SET );
+    if( rt != 0 ){
+        cerr << "File seek error! " << endl;
+        return 1;
+    }
+    rt = fread( buf, sizeof(float), count, file );
+    if( rt != count ) {
+        cerr << "File read error! " << endl;
+        return 1;
+    }
+    return 0;
+}
+
+int WriteChunk( FILE* file, float* buf, long size )
+{
+    long rt = fwrite( buf, sizeof(float), size, file );
+    if( rt != size ) {
+        cerr << "File write size error! " << endl;
+        return 1;
+    }
+    return 0;
+}
+
+float FindThreshold ( int cratio, float* buf, long bufsize )
+{
+    if( bufsize % cratio != 0 )
+        cerr << "Input file not divided by input cratio!" << endl;
+    long n = bufsize / cratio - 1;
+    
+    vector<float> tmp( bufsize, 0.0 );
+    for( long i = 0; i < bufsize; i++ )
+        if( buf[i] > 0 )
+            tmp[i] = buf[i] * -1.0;
+        else
+            tmp[i] = buf[i];
+
+    nth_element( tmp.begin(), tmp.begin()+n, tmp.end() );
+    float nth = tmp[n] * -1.0;
 
     return nth;
 }
 
-void ZeroOut( int cratio, QccWAVSubbandPyramid3D* pyramid )
-{
-    float nth = FindThreshold( cratio, pyramid );
-    float nnth = -1.0 * nth;
-    for (int frame = 0; frame < pyramid->num_frames; frame++)
-        for (int row = 0; row < pyramid->num_rows; row++)
-            for (int col = 0; col < pyramid->num_cols; col++)
-                if( pyramid->volume[frame][row][col] < nth ||
-                    pyramid->volume[frame][row][col] > nnth )
-                    pyramid->volume[frame][row][col] = 0.0;
-
-}
-
 int main( int argc, char* argv[] )
 {
-    QccWAVSubbandPyramid3D pyramid;
-    int cratio;
-    QccString outputSPBT;
-
-    if( QccParseParameters( argc, argv, USG_STRING, pyramid.filename, &cratio, outputSPBT ) )
-        QccErrorExit();
-
-    if( QccWAVSubbandPyramid3DRead( &pyramid ) ) {
-        QccErrorAddMessage( "%s: Read input subband pyramid failed: %s", argv[0], argv[1] );
-        QccErrorExit();
+    if( argc != 4 ) {
+        cerr << "Please specify cratio, and input and output file names! " << endl;
+        exit (1);
     }
+    int cratio = atoi( argv[1] );
+    FILE* input;
+    FILE* output;
+    long nbyte = OpenFileRead( input, argv[2] );
+    OpenFileWrite( output, argv[3] );
 
-/*
-    ZeroOut( cratio, &pyramid );
-*/
-    QccStringCopy( pyramid.filename, outputSPBT );
-    if( QccWAVSubbandPyramid3DWrite( &pyramid ) ){
-        QccErrorAddMessage( "%s: Write output SPBT failed: %s", argv[0], argv[2] );
-        QccErrorExit();
-    }
+    long size = nbyte / 4;
+
+    float* buf = new float[ size ];
+    ReadChunk( input, 0, size, buf );
+    float t = FindThreshold( cratio, buf, size );
+    float nt = t * -1.0;
+    long count = 0;
+    for( long i = 0; i < size; i++ )
+        if( buf[i] < t &&  buf[i] > nt ) {
+            buf[i] = 0.0;
+            count++;
+        }
+    
+    WriteChunk( output, buf, size );
+
+    delete[] buf;
+    fclose( input );
+    fclose( output );
+
+    cerr << "\tThreshold = " << t << endl;
+    cerr << "\tZeroed out numbers = " << count << endl;
 
     return 0;
 }
